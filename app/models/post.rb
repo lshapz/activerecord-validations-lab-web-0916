2c9bbd5f505186a237c415e29767ec09d92ba372
lshapz <laurashapiro01@gmail.com>
2016-10-22 13:35:44 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -14,9 +14,22 @@ validate :clickbait?
 
 
 def clickbait?
-  if CLICKERS.none? {|tite| tite.match title}
+  # if CLICKERS.none? {|tite| tite.match title}
+  #   errors.add(:title, "must be clickbait")
+  # end
+  thing = false
+  CLICKERS.each do |x|
+      if x.match title
+      thing = true
+      break
+    end
+    end
+  if thing != true 
     errors.add(:title, "must be clickbait")
-  end
+  end 
+  
+
+
 end 
 
 end
